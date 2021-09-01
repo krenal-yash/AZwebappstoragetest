6f1646b6de0a30a9398353c2580af963cfeec6c1
krenal-yash <87689980+krenal-yash@users.noreply.github.com>
2021-09-01 12:44:45 +0530

    Update hostingstart-python.py

--- hostingstart-python.py
+++ hostingstart-python.py
@@ -1,8 +1,15 @@
-import sys
-import platform
+#import sys
+#import platform
 
-def application(environ, start_response):
-    start_response(b'200 OK', [(b'Content-Type', b'text/html')])
-    with open ("hostingstart-python.html", "r") as hostingstart_file:
-        hosting = hostingstart_file.read()
-        yield hosting.encode('utf8').replace(b'PYTHON_VERSION', platform.python_version().encode('utf8'))
+#def application(environ, start_response):
+#    start_response(b'200 OK', [(b'Content-Type', b'text/html')])
+#    with open ("hostingstart-python.html", "r") as hostingstart_file:
+#        hosting = hostingstart_file.read()
+#        yield hosting.encode('utf8').replace(b'PYTHON_VERSION', platform.python_version().encode('utf8'))
+        
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "<h1>Hi webapp with storage</h1>"
